59abddde3c7b6828ccdcce266b4fb0134534983b
hirorocky <hirorocky@gmail.com>
2020-04-29 17:32:47 +0900

    change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,17 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    user = User.find(params[:id])
+    message = {
+      type: 'text',
+      text: 'しつもんテスト'
+    }
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+    client.push_message(user.line_user_id, message)
+  end
 end
